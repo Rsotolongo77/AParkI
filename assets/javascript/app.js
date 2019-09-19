52e817a92464f3559cbc84225b5fafe00fbb2a7a
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-09-19 08:25:53 -0400

    added address submission form, more css styling.

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 28.589475, lng: -81.199879 },
+        zoom: 8
+    });
+}
+
+initMap();

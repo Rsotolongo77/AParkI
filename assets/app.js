f01d0719ee60475a7857e80c194518e1f403d2f6
Chad Kaplan <chadkaplan@knights.ucf.edu>
2019-09-19 13:32:40 -0400

    Adding the first ajax call that gives back a response with a longitude and lattitude formatted location

--- assets/app.js
+++ assets/app.js
@@ -1,34 +1,42 @@
-    
-            var map;
-            function initMap() {
-                map = new google.maps.Map(document.getElementById('map'), {
-                    center: { lat: 28.589475, lng: -81.199879 },
-                    zoom: 8
-                });
-            }
 
-            initMap();
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 28.589475, lng: -81.199879 },
+        zoom: 8
+    });
+}
 
-            let address;
-            let state;
-            let city;
-            let zipCode;
+initMap();
 
-            $("#submitButton").on("click", function (event) {
-                event.preventDefault();
-                address = $("#address").val().trim();
-                state = $("#state").val().trim();
-                city = $("#city").val().trim();
-                zipCode = $("#zip_code").val().trim();
-                console.log(address);
-                console.log(state);
-                console.log(city);
-                console.log(zipCode);
-                $("#address").val("");
-                $("#state").val("");
-                $("#city").val("");
-                $("#zip_code").val("");
-            })
+let address;
+let state;
+let city;
+let zipCode;
+
+$("#submitButton").on("click", function (event) {
+    event.preventDefault();
+    address = $("#address").val().trim();
+    state = $("#state").val().trim();
+    city = $("#city").val().trim();
+    zipCode = $("#zip_code").val().trim();
+    console.log(address);
+    console.log(state);
+    console.log(city);
+    console.log(zipCode);
+    $("#address").val("");
+    $("#state").val("");
+    $("#city").val("");
+    $("#zip_code").val("");
+})
+
+var queryURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyAl_dAteSxbSnf4wX8cFpQYhpP9dZN35TE&input=Amway&inputtype=textquery&fields=name,geometry,formatted_address";
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+});
 
             // Take the values from search 
             // Throw them into an ajax call api/places/Text
@@ -43,6 +51,6 @@
 
 
 
-        
 
-        
+
+

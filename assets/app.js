552bf479d55079ec9e6cb1d3f0897a84e156cf0c
lethalSid <senien.hicks@gmail.com>
2019-09-20 12:43:03 -0400

    directions nested beside parking radius in 1st ajax call

--- assets/app.js
+++ assets/app.js
@@ -37,23 +37,23 @@ $("#submitButton").on("click", function (event) {
     console.log(address);
 
 
-var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyAl_dAteSxbSnf4wX8cFpQYhpP9dZN35TE&input=" + address + "&inputtype=textquery&fields=name,geometry,formatted_address,icon";
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyAl_dAteSxbSnf4wX8cFpQYhpP9dZN35TE&input=" + address + "&inputtype=textquery&fields=name,geometry,formatted_address,icon";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    let data = response.candidates[0];
-    console.log(data.geometry.location);
-    console.log(data.geometry.location.lat);
-    console.log(data.geometry.location.lng);
-    lat = data.geometry.location.lat;
-    lng = data.geometry.location.lng;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        let data = response.candidates[0];
+        console.log(data.geometry.location);
+        console.log(data.geometry.location.lat);
+        console.log(data.geometry.location.lng);
+        lat = data.geometry.location.lat;
+        lng = data.geometry.location.lng;
 
 
 
     var queryParking = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," +lng + "&key=AIzaSyAl_dAteSxbSnf4wX8cFpQYhpP9dZN35TE&radius=1000&types=parking";
-
+    //inside the 1st .then
     $.ajax({
         url: queryParking,
         method: "GET"
@@ -74,21 +74,26 @@ $.ajax({
         console.log(parking);
         initMap();
     })
+    //inside the 1st then
 
-});
+    var queryDirections = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=AIzaSyAl_dAteSxbSnf4wX8cFpQYhpP9dZN35TE"
 
-$("#address").val("");
+    $.ajax ({
+        url: queryDirections,
+        method: "GET"
+    }).then(function(response) {
+        //logging directions to the console
+        console.log(response);
+    })
+    //1st then ending
+    });
+
+    $("#address").val("");
 })
 
-            // Take the values from search
-            // Throw them into an ajax call api/places/Text
-            // Go through response into formated_address and get back...
-            // Geometry (lattitude and longitude)
             // Set the lat and long into variables
             // take that information and do another ajax call
-            // ajax call for api/places/nearby with a radius of 3000 and a "type: parking"
             // Take that info and plug in into another ajax call for api/maps
-            // Find a way to create markers at the location
 
 
 
